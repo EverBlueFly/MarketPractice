fd08f5929075b57e7c32e085a471eaa3358788d5
EverBlueFly <flyfly30507@yahoo.com.tw>
2019-03-21 17:44:15 +0800

    加入專案檔案。

---
+++ MarketPractice/Controllers/MarketController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using MarketPractice.Entity;
+
+namespace MarketPractice.Controllers
+{
+    public class MarketController : Controller
+    {
+        // GET: Market
+        public ActionResult Index(List<ProductList> sp)
+        {
+            using (MarketDBEntities db = new MarketDBEntities())
+            {
+                var DS = (from pl in db.ProductList
+                          select pl);
+                return View(DS.ToList());
+            }
+        }
+        public ActionResult AddCart(string itemId)
+        {
+            try
+            {
+                using (MarketDBEntities db = new MarketDBEntities())
+                {
+                    
+                if (!string.IsNullOrEmpty(itemId))
+                {
+                    string sUserId = "KrisIdoit";
+                    //string sUserId = Session["UserId"].ToString();
+                    if (string.IsNullOrEmpty(sUserId))
+                    {
+                        return View("Index");
+                    }
+                    
+                        var itDtl = (from pl in db.ProductList
+                                     where pl.itemid == itemId
+                                     select pl).FirstOrDefault();
+                        if (itDtl.left_amount != 0)
+                        {
+                            itDtl.left_amount = itDtl.left_amount - 1;
+
+                            var spit = (from sp in db.ShoppingList
+                                        where sp.itemId == itemId && sp.UserId == sUserId
+                                        select sp).FirstOrDefault();
+                            if (spit != null)
+                            {
+                                spit.amount = spit.amount + 1;
+                            }
+                            else
+                            {
+                                Entity.ShoppingList insToSp = new ShoppingList();
+                                insToSp.itemId = itemId;
+                                insToSp.UserId = sUserId;
+                                insToSp.amount = 1;
+                                db.ShoppingList.Add(insToSp);
+                            }
+                            db.SaveChanges();
+                        }
+                    }
+                    var ItDS = (from pl in db.ProductList
+                                select pl);
+
+                    return View("Index",ItDS.ToList());
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
